122b117bfba6f06d5dc156acb64a9c7c4f4e6f74
Yatin Sonar <97775218+yatinsonar5@users.noreply.github.com>
2023-02-03 20:43:06 +0530

    UrlData Added

--- index.js
+++ index.js
@@ -74,8 +74,119 @@ app.post("/api/headerfooter", (req, res) => {
   });
 });
 
-// Image Upload
+//Post Url
+
+app.post("/api/url", (req, res) => {
+  if (!Object.keys(req.body).length) {
+    res.status(400).send({
+      code: 400,
+      status: "Bad Request",
+      message: "Please send some Valid Data",
+    });
+    return;
+  }
+  let urlData = req.body;
+  fs.writeFile("./text/url.txt", JSON.stringify(urlData), (err) => {
+    if (err) {
+      res.status(500).send({
+        code: 500,
+        status: "Internal Server Error",
+        message: "Failed to send URL data",
+      });
+      return;
+    }
+    res.send({
+      code: 200,
+      status: "Success",
+      message: "Url Data send Successfully",
+      data: urlData,
+    });
+  });
+});
+
+// Get UrlData
+
+app.get("/api/url", (req, res) => {
+  fs.readFile("./text/url.txt", "utf-8", (err, urlData) => {
+    if (err) {
+      res.status(500).send({
+        code: 500,
+        status: "Internal Server Error",
+        message: "Failed to read URL data",
+      });
+      return;
+    }
+    if (!urlData) {
+      res.status(400).send({
+        code: 400,
+        status: "Bad Request",
+        message: "UrlData is not Availble, file is empty",
+      });
+      return;
+    }
+    res.status(200).send({
+        code: 200,
+        status: "Success",
+        message: "Fetched Url Data Successfully",
+        data: JSON.parse(urlData),
+      });
+  });
+});
+
+// Image Upload on S3 bucket
+
+// const AWS = require("aws-sdk");
+// AWS.config.update({
+//   accessKeyId: "YOUR_ACCESS_KEY_ID",
+//   secretAccessKey: "YOUR_SECRET_ACCESS_KEY",
+// });
+// const s3 = new AWS.S3();
+
+// const multer = require("multer");
+
+// const storage = multer.memoryStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, "");
+//   },
+//   filename: (req, file, cb) => {
+//     cb(null, `${Date.now().toString()}-${file.originalname}`);
+//   },
+// });
+
+// const upload = multer({ storage: storage }).single("image");
+
+// app.post("/api/upload", upload, (req, res) => {
+//   const file = req.file;
+
+//   const params = {
+//     Bucket: "YOUR_BUCKET_NAME",
+//     Key: file.filename,
+//     Body: file.buffer,
+//   };
+
+//   s3.upload(params, (error, data) => {
+//     if (error) {
+//       console.log(error);
+//       return res.status(500).send({
+//         code: 500,
+//         status: "Internal Server Error",
+//         message: "Not able to upload image, Please try again.",
+//       });
+//     }
+//     console.log(data);
+//     return res.status(200).send({
+//       code: 200,
+//       status: "Success",
+//       message: "Image has been uploaded",
+//       data: data,
+//     });
+//   });
+// });
+
+//Image Upload on Local Disk
+
 const multer = require("multer");
+
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "./images/");
