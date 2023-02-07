309c04eeabaef375b95226d2a7627a09bc0c926c
Yatin Sonar <97775218+yatinsonar5@users.noreply.github.com>
2023-02-07 14:51:19 +0530

    login fix

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
-const MongoURI = process.env.MongoURI
+const MongoURI = process.env.MongoURI;
 const LOCALURI = process.env.LOCALURI;
 
 mongoose.connect(MongoURI, { useNewUrlParser: true });
@@ -342,113 +342,114 @@ app.post("/api/upload", upload.single("image"), (req, res) => {
 
 //Login
 
-// const jwt = require("jsonwebtoken");
-// app.use(express.json());
-
-// const SECRET_KEY = "yoursecretkey";
-
-// const users = [
-//   {
-//     userId: 1,
-//     username: "user1",
-//     password: "password1",
-//   },
-//   {
-//     userId: 2,
-//     username: "user2",
-//     password: "password2",
-//   },
-// ];
-
-// // Route to handle login
-// app.post("/api/login", (req, res) => {
-//   const { username, password } = req.body;
-
-//   // Check if user exists
-//   const user = users.find(
-//     (u) => u.username === username && u.password === password
-//   );
-//   if (!user) {
-//     return res.status(401).send({
-//       code: 401,
-//       status: "Unauthorized",
-//       error: "Incorrect username or password",
-//     });
-//   }
-//   // Create JWT token
-//   const token = jwt.sign({ userId: user.userId }, SECRET_KEY, {
-//     expiresIn: "1h",
-//   });
-//   // Return token in response
-//   res.send({
-//     code: 200,
-//     status: "Success",
-//     userId: user.userId,
-//     token,
-//   });
-// });
-
 const jwt = require("jsonwebtoken");
-const secretKey = "your-secret-key";
-const User = require("./models/login_model");
+app.use(express.json());
 
+const SECRET_KEY = "yoursecretkey";
+
+const users = [
+  {
+    userId: 1,
+    username: "user1",
+    password: "password1",
+  },
+  {
+    userId: 2,
+    username: "user2",
+    password: "password2",
+  },
+];
+
+// Route to handle login
 app.post("/api/login", (req, res) => {
-  User.findOne({ username: req.body.username }, (error, user) => {
-    if (error) {
-      res.status(500).send({
-        code: 500,
-        status: "Internal Server Error",
-        message: "Not able to find User",
-      });
-    } else if (!user) {
-      const newUser = new User({
-        username: req.body.username,
-        password: req.body.password,
-      });
-      newUser.save((error) => {
-        if (error) {
-          res.status(500).send(error);
-        } else {
-          const token = jwt.sign({ id: newUser._id }, secretKey, {
-            expiresIn: "1h",
-          });
-          res.status(200).send({
-            code: 200,
-            status: "Success",
-            message: "User Created Successfully",
-            userId: newUser._id,
-            token,
-          });
-        }
-      });
-    } else if (
-      user.username == req.body.username &&
-      user.password !== req.body.password
-    ) {
-      res.status(401).send({
-        code: 401,
-        status: "Unauthorized",
-        message: "Incorrect Password",
-      });
-    } else if (
-      user.username === req.body.username &&
-      user.password === req.body.password
-    ) {
-      const token = jwt.sign({ id: user._id }, secretKey, {
-        expiresIn: "1h",
-      });
-      res.status(200).send({
-        code: 200,
-        status: "Success",
-        message: "User LoggedIn Successfully",
-        userId: user._id,
-        token,
-      });
-    }
+  const { username, password } = req.body;
+
+  // Check if user exists
+  const user = users.find(
+    (u) => u.username === username && u.password === password
+  );
+  if (!user) {
+    return res.status(401).send({
+      code: 401,
+      status: "Unauthorized",
+      message: "Incorrect username or password",
+    });
+  }
+  // Create JWT token
+  const token = jwt.sign({ userId: user.userId }, SECRET_KEY, {
+    expiresIn: "1h",
+  });
+  // Return token in response
+  res.send({
+    code: 200,
+    status: "Success",
+    message: "User LoggedIn Successfully",
+    userId: user.userId,
+    token,
   });
 });
 
-const port = process.env.PORT;
+// const jwt = require("jsonwebtoken");
+// const secretKey = "your-secret-key";
+// const User = require("./models/login_model");
+
+// app.post("/api/login", (req, res) => {
+//   User.findOne({ username: req.body.username }, (error, user) => {
+//     if (error) {
+//       res.status(500).send({
+//         code: 500,
+//         status: "Internal Server Error",
+//         message: "Not able to find User",
+//       });
+//     } else if (!user) {
+//       const newUser = new User({
+//         username: req.body.username,
+//         password: req.body.password,
+//       });
+//       newUser.save((error) => {
+//         if (error) {
+//           res.status(500).send(error);
+//         } else {
+//           const token = jwt.sign({ id: newUser._id }, secretKey, {
+//             expiresIn: "1h",
+//           });
+//           res.status(200).send({
+//             code: 200,
+//             status: "Success",
+//             message: "User Created Successfully",
+//             userId: newUser._id,
+//             token,
+//           });
+//         }
+//       });
+//     } else if (
+//       user.username == req.body.username &&
+//       user.password !== req.body.password
+//     ) {
+//       res.status(401).send({
+//         code: 401,
+//         status: "Unauthorized",
+//         message: "Incorrect Password",
+//       });
+//     } else if (
+//       user.username === req.body.username &&
+//       user.password === req.body.password
+//     ) {
+//       const token = jwt.sign({ id: user._id }, secretKey, {
+//         expiresIn: "1h",
+//       });
+//       res.status(200).send({
+//         code: 200,
+//         status: "Success",
+//         message: "User LoggedIn Successfully",
+//         userId: user._id,
+//         token,
+//       });
+//     }
+//   });
+// });
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
